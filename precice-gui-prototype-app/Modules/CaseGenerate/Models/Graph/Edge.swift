6f1a25746b64735745d6c6775a7809815ba85a14
Orlando <orlac2700@gmail.com>
2026-09-26 08:25:25 +0900

    Update Edge.swift
    REmove unnecessary attribute. It was created in an attempt to uncross edges, but that was not it.

--- precice-gui-prototype-app/Modules/CaseGenerate/Models/Graph/Edge.swift
+++ precice-gui-prototype-app/Modules/CaseGenerate/Models/Graph/Edge.swift
@@ -14,7 +14,6 @@ struct Edge: Identifiable, Codable {
     var data: String            // "The data that is exchanged"
     var dataType: DataType?     // scalar or vector
     var strength: EdgeStrength  // "Weak" or "Strong"
-    var createdAt: Date = Date()
     
     init(id: UUID = UUID(),
          sourceLocationNodeId: UUID,
